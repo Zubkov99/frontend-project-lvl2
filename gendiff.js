a9eea679de232ed638cfb565824f4ef6dcf1e131
DimaZ <dimazubkov99@gmail.com>
2021-12-15 18:12:10 +0300

    done second step

--- gendiff.js
+++ gendiff.js
@@ -4,7 +4,8 @@ import { Command } from 'commander';
 const program = new Command();
 program
 .version('0.0.1', '-v, --vers', 'output the current version')
-.description('Compares two configuration files and shows a difference.', '-h');
-
+.description('Compares two configuration files and shows a difference.', '-h')
+.option('-f --format [type]', 'output format')
+.argument('<filepath1> <filepath2>');
 
 program.parse(process.argv);
