343520397c89459ea47ef92a8950e9d734621aca
DimaZ <dimazubkov99@gmail.com>
2022-01-18 00:26:20 +0300

    fix readFiles.js

--- src/formatters/formatRendering.js
+++ src/formatters/formatRendering.js
@@ -10,15 +10,12 @@ const mapper = {
 };
 
 const formatRendering = (format, tree) => {
-  console.log(format);
   switch (format) {
     case 'plain':
       return mapper.plain(tree);
     case 'json':
       return mapper.json(tree);
     case 'stylish':
-      return mapper.stylish(tree);
-    case undefined:
       return mapper.stylish(tree);
     default:
       throw new Error(`Format error ${format}`);
